23ae5d4a6d72e4ddd672c2780c2a72ea62d0bb20
msmolka-wsei <michal.smolka@microsoft.wsei.edu.pl>
2021-06-05 15:20:08 +0200

    video

--- js/main.js
+++ js/main.js
@@ -59,3 +59,13 @@ function move2() {
 move1();
 move2();
 move3();
+
+$('.video').parent().click(function () {
+  if($(this).children(".video").get(0).paused){
+      $(this).children(".video").get(0).play();
+      $(this).children(".playpause").fadeOut();
+  }else{
+     $(this).children(".video").get(0).pause();
+      $(this).children(".playpause").fadeIn();
+  }
+});
